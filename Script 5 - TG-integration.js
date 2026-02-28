21b8aa3ca4cac1e8f29a2ea020d82472eb4309d9
Roman <vakitzahi@gmail.com>
2026-02-28 20:36:05 +0300

    pollish(4)

--- Script 5 - TG-integration.js
+++ Script 5 - TG-integration.js
@@ -1,35 +1,29 @@
 function main() {
-  // Получаем все активные кампании
   var campaignIterator = AdsApp.campaigns()
-    .withCondition("Status = 'PAUSED'")  // Фильтруем по активным кампаниям
+    .withCondition("Status = 'PAUSED'")
     .get();
 
-  // Перебираем кампании и проверяем их статус
   while (campaignIterator.hasNext()) {
     var campaign = campaignIterator.next();
 
-    // Проверяем, если кампания приостановлена
     if (!campaign.isEnabled()) {
       sendTelegramMessage('Кампания приостановлена: ' + campaign.getName());
     }
 
-    // Проверяем, если кампания достигла суточного бюджета
     var todayStats = campaign.getStatsFor("TODAY");
     if (todayStats.getCost() >= campaign.getBudget().getAmount()) {
       sendTelegramMessage('Кампания достигла суточного бюджета: ' + campaign.getName());
     }
   }
 }
-
+//Настройки Telegram-бота
 function sendTelegramMessage(message) {
   var botToken = 'СЮДА ТОКЕН БОТА'; // Замените на ваш токен бота
   var chatId = 'СЮДА CHAT-ID';     // Замените на ваш chat_id
   
-  // Экранируем параметры, чтобы избежать проблем с URL
   var encodedMessage = encodeURIComponent(message);
   var telegramUrl = "https://api.telegram.org/bot" + botToken + "/sendMessage?chat_id=" + chatId + "&text=" + encodedMessage;
 
-  // Отправляем запрос в Telegram API
   try {
     var response = UrlFetchApp.fetch(telegramUrl);
     Logger.log("Уведомление отправлено: " + message);
@@ -37,3 +31,4 @@ function sendTelegramMessage(message) {
     Logger.log("Ошибка при отправке уведомления: " + e.message);
   }
 }
+
